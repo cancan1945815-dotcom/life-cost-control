b46e3805e99e44d215f7b6121683bb9f6ebd91fa
草莓小熊 <zhangxinhui@zhangxinhuideMacBook-Air.local>
2026-03-08 15:18:38 +0800

    fix vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-VitePWA({
-  registerType: "prompt",
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
-  manifest: {
-    name: "Life Cost App",
-    short_name: "LifeCost",
-    theme_color: "#ffffff",
-    background_color: "#ffffff",
-    display: "standalone",
-    start_url: "/"
-  },
-
-  workbox: {
-    skipWaiting: true,
-    clientsClaim: true
-  }
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "prompt",
+      manifest: {
+        name: "Life Cost App",
+        short_name: "LifeCost",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/"
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+  ]
 })
